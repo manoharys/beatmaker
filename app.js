831b6829c0103cb1f7fc95ae9f0806f3bf156610
manohar <manumanohar1527@gmail.com>
2020-03-22 07:27:06 -0700

    updated animations

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ window.addEventListener('load', () => {
     const pads = document.querySelectorAll('.pads div');
     const visual = document.querySelector(".visual");
     const colors = [
-        "#60d394",
-        "#d36060",
-        "#c060d3",
-        "#d3d160",
-        "#606bd3",
-        "#60c2d3"
+        "orangered",
+        "rgb(212, 0, 255)",
+        "rgb(255, 238, 0)",
+        "rgb(4, 0, 255)",
+        "rgba(0, 255, 149, 0.767)",
+        "rgb(99, 73, 64)"
     ];
 
     //Adding a eventlistner
